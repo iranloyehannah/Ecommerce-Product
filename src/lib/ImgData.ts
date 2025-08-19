4eb6871fe6212dfc2221eeccfbfe6a91a3d87364
Iranloye Hannah <iranloye8.hannah@gmail.com>
2025-08-19 20:40:45 +0100

    👩🏽<200d>🔧: Import Product Images

--- src/lib/ImgData.ts
+++ src/lib/ImgData.ts
@@ -1,3 +1,13 @@
+// ==================== import Images ==================== 
+import img1 from "./src/assets/images/image-product-1.jpg";
+import img1Thumb from "./src/assets/images/image-product-1-thumbnail.jpg";
+import img2 from "./src/assets/images/image-product-2.jpg";
+import img2Thumb from "./src/assets/images/image-product-2-thumbnail.jpg";
+import img3 from "./src/assets/images/image-product-3.jpg";
+import img3Thumb from "./src/assets/images/image-product-3-thumbnail.jpg";
+import img4 from "./src/assets/images/image-product-4.jpg";
+import img4Thumb from "./src/assets/images/image-product-4-thumbnail.jpg";
+
 export type ImgData = {
   id: number;
   src: string;
@@ -7,26 +17,26 @@ export type ImgData = {
 export const Images: ImgData[] = [
   {
     id: 1,
-    src: "./src/assets/images/image-product-1.jpg",
-    thumbnail: "./src/assets/images/image-product-1-thumbnail.jpg",
+    src: img1,
+    thumbnail: img1Thumb,
     alt: "Image-1",
   } ,
   {
     id: 2,
-    src: "./src/assets/images/image-product-2.jpg",
-    thumbnail: "./src/assets/images/image-product-2-thumbnail.jpg",
+    src: img2,
+    thumbnail: img2Thumb,
     alt: "Image-2",
   } ,
   {
     id: 3,
-    src: "./src/assets/images/image-product-3.jpg",
-    thumbnail: "./src/assets/images/image-product-3-thumbnail.jpg",
+    src: img3,
+    thumbnail: img3Thumb,
     alt: "Image-3",
   } ,
   {
     id: 4,
-    src: "./src/assets/images/image-product-4.jpg",
-    thumbnail: "./src/assets/images/image-product-4-thumbnail.jpg",
+    src: img4,
+    thumbnail: img4Thumb,
     alt: "Image-4",
   } ,
 ]
